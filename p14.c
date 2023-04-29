f1dcc0e409ae0271ffdb582e6ea6d59765cf4401
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-29 22:27:35 +0530

    Create p14.c

---
+++ p14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main(){int n;
+printf("Enter the number =>");
+scanf("%d",&n);
+if (n%2==0){
+    printf("\nNumber is even");
+}
+else {
+    printf("Number is odd");
+}
+getch();
+}
