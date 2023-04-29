9c26b56a2c7ceafde70ae88537083f48d8ef3fc1
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-29 21:58:39 +0530

    Create p9.c

---
+++ p9.c
@@ -0,0 +1,11 @@
+//Swapping Values without using extra variable
+#include <stdio.h>
+main (){ float a,b,c,avg;
+printf("Enter Three Numbers \n");
+scanf("%f",&a);
+scanf("%f",&b);
+scanf("%f",&c);
+avg=(a+b+c)/3;
+printf("The average is %f",avg);
+getch();
+}
