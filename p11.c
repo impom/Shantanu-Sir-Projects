c17ffed505baccfd26641ca51c8dfbeb0e9ee3a3
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-29 22:07:34 +0530

    Create p11.c

---
+++ p11.c
@@ -0,0 +1,12 @@
+//Use of if-else statement
+#include <stdio.h>
+main (){
+int n;
+printf("Enter an integer =>");
+scanf("%d",&n);
+if (n<0)
+    printf("\nNumber is negative");
+else
+printf("\nNumber is positive");
+getch();
+}
