343fde2f8cf8bfca6f58da4e7c798f5aede608ee
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-29 22:04:40 +0530

    Create p10.c

---
+++ p10.c
@@ -0,0 +1,10 @@
+//inch to cm converter
+#include <stdio.h>
+main (){
+float i=2.54,inch,cm;
+printf("Enter the length in inches \n=>");
+scanf("%f",&inch);
+cm=i*inch;
+printf("\n The Required Length in cm is %f",cm);
+getch();
+}
