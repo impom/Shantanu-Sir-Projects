c34f888c618455cc329ed94755d632e2022716e5
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-04-29 21:41:15 +0530

    Update p4.c
    Sum and average of three integer numbers . (Use of typecasting)

--- p4.c
+++ p4.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+main (){
+int n1,n2,n3,sum;
+float avg;
+n1 = 100;
+n2 = 50;
+n3 = 10;
+sum = n1+n2+n3;
+avg = (float)sum/3;
+printf("Sum is %d \n", sum);
+printf("Average is %f ",avg);
+getch();
+}
