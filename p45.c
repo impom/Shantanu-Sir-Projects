257ceed11c591362579f2ac4aafc5272112a59fc
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-10-31 19:39:38 +0530

    Create p45.c

---
+++ p45.c
@@ -0,0 +1,15 @@
+//1!+2!+3!+.........+n!
+#include <stdio.h>
+int main(){
+  int n,i,f=1,j,s=0;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+    f=1;
+    for(j=1;j<=i;j++)
+      {f=f*i;}
+    s=s+f;
+  }
+  printf("The Sum is %d",s);
+  return 0;
+}
