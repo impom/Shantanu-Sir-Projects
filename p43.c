73dd46a4c8807b00f41b46870d059d5d6b79842e
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-10-31 19:26:50 +0530

    Update p43.c

--- p43.c
+++ p43.c
@@ -1,13 +1,17 @@
-//1,1,2,3,5,8,13
-#include <stdio.h>
-int main(){
-  int a=0,b=1,c,i;
-  for(i=1;i<=7;i++){
-    c=a+b;
-    printf("%d,",c);
-    a=b;
-    b=c;
-  }
-  return 0;
-  
+//43. Display the numbers 1,1,2,3,5,8,13 (Fibonacci numbers)
+#include<stdio.h>
+int main()
+{
+      int i=1,j=1,s=0,c=1;
+      printf("%d",i);
+      printf("\n%d",j);
+      while(c<=5)
+      {
+                 s=i+j;
+                 printf("\n%d",s);
+                 i=j;
+                 j=s;
+                 c=c+1;
+      }
+return 0;
 }
