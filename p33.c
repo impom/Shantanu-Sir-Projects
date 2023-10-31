aa40dce0e0d50ada4363784749ff7effb06b2de5
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-10-31 18:13:18 +0530

    Create p33.c

---
+++ p33.c
@@ -0,0 +1,11 @@
+//Factorial of n
+#include <stdio.h>
+int main(){
+  int n,i,f=1;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+    f=f*i;
+  }
+  printf("The factorial of %d is %d",n,f);
+}
