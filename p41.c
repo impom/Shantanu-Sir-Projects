294657fe8cfddbfd6dc913be0d3afcc5000101b1
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-10-31 19:01:30 +0530

    Create p41.c

---
+++ p41.c
@@ -0,0 +1,12 @@
+//Multiplication table
+#include <stdio.h>
+int main(){
+  int i,n,m;
+  printf("Enter the number : ");
+  scanf("%d",&n);
+  for(i=1;i<=10;i++){
+    m=n*i;
+    printf("%d X %d = %d\n"n,i,m);
+  }
+  return 0;
+}
