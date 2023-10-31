18e2287d4d8f75753f2d7d057eb21ee035e8661d
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-10-31 18:08:44 +0530

    Create p32.c

---
+++ p32.c
@@ -0,0 +1,11 @@
+//to find the sum of even numbers between 1-100
+#include <stdio.h>
+int main(){
+  int i=2;s=0;
+  while(i<=100){
+    s=s+i;
+    i=i+2;
+  }
+  printf("Sum is %d",s);
+  return 0;
+}
