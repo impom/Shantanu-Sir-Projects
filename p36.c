fa8b81f8d90e935f2c885023d7ced67a471f969b
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-10-31 18:31:31 +0530

    Update p36.c

--- p36.c
+++ p36.c
@@ -1,1 +1,14 @@
 //It is same as p34.c
+//Input 100 numbers and find the average
+#include <stdio.h>
+int main(){
+   int c,n,s=0,avg;
+  printf("Enter the numbers : ");
+  for(c=1;c<=100;c++){
+    scanf("%d",&n);
+    s=s+n;
+  }
+  avg = s/100;
+  printf("The average of 100 numbers are %d",avg);
+  return 0;
+}
