2d967e3b06ee4694b34f56530d1ef65467cfb377
Rupam Ghosh <117834750+impom@users.noreply.github.com>
2023-10-31 19:50:47 +0530

    Update p48.c

--- p48.c
+++ p48.c
@@ -9,9 +9,9 @@ print
 #include <stdio.h>
 int main(){
   int i,j,k=1;
-  for(i=1;i<=5;i++){
+  for(i=1;i<=4;i++){
     for(j=1;j<=i;j++){
-      printf("%d",k);
+      printf("%d ",k);
       k++;
     }
     printf("\n");
